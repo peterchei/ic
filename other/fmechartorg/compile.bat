aee3d1b31cf2f761537acf918ac18a58e3481e97
peterchei <peterchei@outlook.com>
2015-12-05 10:53:16 +0800

    refactor and clean code

--- other/fmechartorg/compile.bat
+++
@@ -1,3 +0,0 @@
-set path = E:\jdk1.1.8\bin;E:\jdk1.1.8
-
-E:\jdk1.1.8\bin\javac ChartData.java ChartScreen.java ChartSource.java ChartUIObject.java FAction.java FBar.java FButtonBar.java FIButton.java FLine.java FMEChartA.java FMEChartAppletA.java FMenuBar.java FPoint.java ScreenActionListener.java TemplateApplet.java
